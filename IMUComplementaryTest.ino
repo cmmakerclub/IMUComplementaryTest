47def68ddd6e721ba6e541d57b43516669cb6375
Nat Weerawan <nat.wrw@gmail.com>
2016-04-23 02:50:16 +0700

    For lambda!

--- IMUComplementaryTest.ino
+++ IMUComplementaryTest.ino
@@ -20,33 +20,78 @@ LSM9DS1 imu;
 #define M_PI 3.14159265359
 #define dt (10.0/1000.0)             // 100hz = 10ms
 
-#define RMotor_offset 60 // The offset of the Motor
-#define LMotor_offset 50 // The offset of the Motor
+#define RMotor_offset 10 // The offset of the Motor
+#define LMotor_offset 00 // The offset of the Motor
 
 // walkin
-static float kp = 70.00f;
-static float ki = 2.500f;
-static float kd = 6000.00f;
+static float kp = 720.00f;
+static float kd = 500.00f;
+static float ki = 0.0f;
 
 uint32_t _prev = millis();
+static bool system_running = false;
 
 static uint32_t lastTime = millis();
 
 int TN1 = 4;
 int TN2 = 3;
-int ENA = 5;
+
 int TN3 = 8;
 int TN4 = 7;
-int ENB = 6;
+
+int ENA = 11;
+int ENB = 12;
 
 static float error = 0;  // Proportion
 static float errSum = 0;
 static float dErr = 0;
 
 struct Motor {
-  float LOutput;
-  float ROutput;
+  float Output;
 } motor_t;
+
+void myPID(float , struct Motor *);
+
+void backward() {
+  digitalWrite(TN1, 1);
+  digitalWrite(TN2, 0);
+
+  digitalWrite(TN3, 0);
+  digitalWrite(TN4, 1);
+}
+
+void forward() {
+  digitalWrite(TN1, 0);
+  digitalWrite(TN2, 1);
+
+  digitalWrite(TN3, 1);
+  digitalWrite(TN4, 0);
+
+}
+
+void PWMControl(float Output)
+{
+  if (Output > 0) {
+    forward();
+  }
+  else if (Output < 0)
+  {
+    backward();
+  } else {
+    digitalWrite(ENA, 0);
+    digitalWrite(ENB, 0);
+  }
+
+
+// Serial.println(Output);
+
+  // Serial.println(min(200, (abs(Output) + LMotor_offset)));
+  analogWrite(ENA, min(200, (abs(Output) + LMotor_offset)));
+  analogWrite(ENB, min(200, (abs(Output) + RMotor_offset)));
+  // analogWrite(ENA, abs(Output) + LMotor_offset);
+  // analogWrite(ENB, abs(Output) + RMotor_offset);
+
+}
 
 
 //http://www.pieter-jan.com
@@ -77,41 +122,62 @@ void calibrateIMU();
 void readIMUSensor(float *Angle_Filtered);
 void setup() {
   Serial.begin(115200); // initialize Serial communication
+  pinMode(LED_BUILTIN, OUTPUT);
 
+  // while(true) {
+  //   forward();
+  //   delay(500);
+  // //   backward();
+  // //   delay(500);
+  // }
+  // //
 
   // initialize device
   Serial.println("Initializing IMU device...");
   calibrateIMU();
+  system_running = true;
 
   // configure Arduino LED for activity indicator
-  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
   static Motor motor;
-  static float Angle_Filtered;
-  if ( millis() - _prev > dt * 1000 ) {
+  static float pitch_value_filtered;
+  if (system_running && ((millis() - _prev) > 10)){
+    readIMUSensor(&pitch_value_filtered);
     _prev = millis();
-    readIMUSensor(&Angle_Filtered);
     //   If angle > 45 or < -45 then stop the robot
-    if (abs(Angle_Filtered) < 45)
-    {
-      //      myPID(Angle_Filtered, &motor);
-      //      PWMControl(motor.LOutput, motor.ROutput);
+    if (abs(pitch_value_filtered) < 300) {
+      myPID(pitch_value_filtered, &motor);
+      PWMControl(motor.Output);
     }
     else
     {
+      system_running = false;
       //TODO:// set zero when robot down.
-      //      Output = error = errSum = dErr = 0;
-      Serial.println("STOP");
+          //  Output = error = errSum = dErr = 0;
+      // Serial.println("STOP");
       analogWrite(ENA, 0);  // Stop the wheels
       analogWrite(ENB, 0);  // Stop the wheels
     }
-    blinkState = !blinkState;
-    digitalWrite(LED_BUILTIN, blinkState);
   }
-
 }
+
+void myPID(float filtered_angle, struct Motor *motor) {
+  static float lastErr = 0;
+  static float Output;
+  uint32_t timeChange = (millis() - lastTime);
+  lastTime = millis();
+
+  error = filtered_angle;  // Proportion
+  errSum += error * timeChange;  // Integration
+  dErr = (error - lastErr) / timeChange;  // Differentiation
+  Output = kp * error + ki * errSum + kd * dErr;
+  lastErr = error;
+
+  motor->Output = Output;
+}
+
 
 void readIMUSensor(float *Angle_Filtered) {
   static float roll;
@@ -119,8 +185,7 @@ void readIMUSensor(float *Angle_Filtered) {
   // put your main code here, to run repeatedly:
   int accData[3];
   int gyrData[3];
-  //  CurieIMU.readMotionSensor(ax, ay, az, gx, gy, gz);
-//  CurieIMU.readMotionSensor(accData[0], accData[1], accData[2], gyrData[0], gyrData[1], gyrData[2]);
+
   imu.readAccel();
   imu.readGyro();
 
@@ -135,7 +200,7 @@ void readIMUSensor(float *Angle_Filtered) {
   ComplementaryFilter(accData, gyrData, &pitch, &roll);
 
   *Angle_Filtered = pitch;
-  //  Serial.println(String("Angle Filter = ") + String(pitch));
+  // //  Serial.println(String("Angle Filter = ") + String(pitch));
   Serial.println(pitch);
 }
 
